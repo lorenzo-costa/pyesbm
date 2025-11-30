ded9c2d14ced94b32c1e9add81b5090551b6cf74
Lorenzo Costa <lorenzocosta2001@gmail.com>
2025-11-30 11:37:34 -0500

    commit old functions

---
+++ pyesbm/utilities/numba_functions.py
@@ -0,0 +1,376 @@
+###########################################
+# this file contains a bunch of numba optimised functions 
+# to compute stuff
+##############################################
+
+import numba as nb
+from math import lgamma
+import numpy as np
+from .gpu_functions import compute_prob_gpu
+
+###########################################
+@nb.jit(nopython=True, fastmath=True)
+def compute_log_likelihood(a, 
+                           b, 
+                           nh, 
+                           nk, 
+                           eps, 
+                           mhk, 
+                           user_clustering, 
+                           item_clustering, 
+                           dg_u, 
+                           dg_i, 
+                           dg_cl_u, 
+                           dg_cl_i, 
+                           degree_param_users=0.5, 
+                           degree_param_items=0.5, 
+                           degree_corrected=False):
+    """Function to compute log-likelihood of current clustering
+
+    To understand the parameters, see the detailed descriptions in
+    results/text/lorenzocosta_thesis.pdf.
+    
+    Parameters
+    ----------
+    a : float
+        Shape parameter for gamma prior
+    b : float
+        Rate parameter for gamma prior
+    nh : array-like
+        User cluster sizes
+    nk : array-like
+        Item cluster sizes
+    eps : float
+        Small value to avoid division by zero
+    mhk : array-like
+        mhk matrix
+    user_clustering : array-like
+        User clustering assignments
+    item_clustering : array-like
+        Item clustering assignments
+    dg_u : array-like
+        User degree sequence
+    dg_i : array-like
+        Item degree sequence
+    dg_cl_u : array-like
+        Sum of user degrees for users within each cluster
+    dg_cl_i : array-like
+        Sum of item degrees for items within each cluster
+    degree_param_users : float, optional
+        Degree-correction parameter for users, by default 0.5
+    degree_param_items : float, optional
+        Degree-correction parameter for items, by default 0.5
+    degree_corrected : bool, optional
+        Whether to apply degree correction, by default False
+
+    Returns
+    -------
+    out : float
+        log-likelihood value
+    """
+    
+    out = 0.0
+    if degree_corrected is True:
+        for h in range(len(nh)):
+            idx = np.where(user_clustering==h)[0]
+            for i in idx:
+                out += lgamma(degree_param_users+dg_u[i]+eps)
+            out -= lgamma(nh[h]*degree_param_users+dg_cl_u[h]+eps)
+            out += (dg_cl_u[h]*np.log(nh[h]))
+            out += lgamma(nh[h]*degree_param_users)
+            out -= (nh[h]*lgamma(degree_param_users))
+            
+        for k in range(len(nk)):
+            idx = np.where(item_clustering==k)[0]
+            for i in idx:
+                out += lgamma(degree_param_items+dg_i[i]+eps)
+            out -= lgamma(nk[k]*degree_param_items+dg_cl_i[k]+eps)
+            out += (dg_cl_i[k]*np.log(nk[k]))
+            out += lgamma(nk[k]*degree_param_items)
+            out -= (nk[k]*lgamma(degree_param_items))
+                                
+    for h in range(len(nh)):
+        for k in range(len(nk)):
+            out += lgamma(mhk[h, k]+a+eps)-(mhk[h,k]+a)*np.log((nh[h]*nk[k]+b))
+            
+    return out
+
+
+###################################
+# gibbs-type prior sampling scheme
+####################################
+
+@nb.jit(nopython=True)
+def sampling_scheme(V, H, frequencies, bar_h, scheme_type, scheme_param, sigma, gamma):
+    """Probability of sampling each cluster (and a new one) under Gibbs-type priors.
+
+    Parameters
+    ----------
+    V : int
+        number of data points (nodes)
+    H : int
+        number of clusters
+    frequencies : array-like
+        cluster frequencies
+    bar_h : int
+        maximum number of clusters (for DM prior)
+    scheme_type : str
+        type of the prior distribution
+    scheme_param : float
+        additional parameter for Gibbs-type priors
+    sigma : float
+        sigma parameter for Gibbs-type priors
+    gamma : float
+        additional parameter for the GN model
+
+    Returns
+    -------
+    probs : array-like
+        probabilities of sampling each cluster and a new cluster
+
+    """
+    
+    if scheme_type == 'DM':
+        if H < bar_h:
+            probs = np.zeros(len(frequencies)+1)
+            for i in range(len(frequencies)):
+                probs[i] = frequencies[i]-sigma
+            probs[-1] = -sigma*(bar_h-H)
+        else:
+            probs = np.zeros(len(frequencies))
+            for i in range(len(frequencies)):
+                probs[i] = frequencies[i]-sigma
+
+    if scheme_type == 'DP':
+        probs = np.zeros(len(frequencies)+1)
+        for i in range(len(frequencies)):
+            probs[i] = frequencies[i]
+        probs[-1] = scheme_param
+
+    if scheme_type == 'PY':
+        probs = np.zeros(len(frequencies)+1)
+        for i in range(len(frequencies)):
+            probs[i] = frequencies[i]-sigma
+        probs[-1] = scheme_param+H*sigma
+
+    if scheme_type == 'GN':
+        probs = np.zeros(len(frequencies)+1)
+        for i in range(len(frequencies)):
+            probs[i] = (frequencies[i]+1)*(V-H+gamma)
+        probs[-1] = H*(H-gamma)
+    
+    return probs
+
+
+#################################
+# log probability computation for gibbs sampling steps
+#################################
+
+# main function picking implementation
+def compute_log_prob(probs, 
+                     mhk_minus, 
+                     frequencies_primary_minus, 
+                     frequencies_secondary,
+                     y_values, 
+                     epsilon, 
+                     a, 
+                     b, 
+                     max_clusters, 
+                     is_user_mode, 
+                     degree_corrected,
+                     degree_param, 
+                     degree_cluster_minus, 
+                     degree_node, 
+                     device):
+    """Function to compute log probabilities for Gibbs sampling steps.
+    
+    Wrapper to choose between CPU and GPU implementations. To understand the parameters, 
+    see the detailed descriptions in results/text/lorenzocosta_thesis.pdf.
+
+    Parameters
+    ----------
+    probs : array-like
+        Array of probabilities.
+    mhk_minus : array-like
+        mhk matrix with user/item removed.
+    frequencies_primary_minus : array-like
+        Frequencies of users/items with user/item removed.
+    frequencies_secondary : array-like
+        Frequencies of items/users (opposite of primary).
+    y_values : array-like
+        Array of yih or yuk values.
+    epsilon : float
+        Small value to avoid division by zero.
+    a : float
+        Shape parameter for gamma prior.
+    b : float
+        Rate parameter for gamma prior.
+    max_clusters : int
+        Current number of clusters.
+    is_user_mode : bool
+        Flag indicating user mode.
+    degree_corrected : bool
+        Flag indicating if degree correction is applied.
+    degree_param : float
+        Degree-correction parameter.
+    degree_cluster_minus : array-like
+        Sum of degrees in the cluster with user/item removed.
+    degree_node : float
+        Degree of selected node.
+    device : str
+        Device to run the computations on ('cpu' or 'gpu').
+
+    Returns
+    -------
+    log_probs : array-like
+        Computed log probabilities.
+    """
+    
+    if device == 'gpu':
+        try:
+            return compute_prob_gpu(probs, mhk_minus, frequencies_primary_minus, frequencies_secondary,
+                                         y_values, epsilon, a, b, max_clusters, degree_corrected,
+                                         degree_cluster_minus, degree_node, degree_param, is_user_mode)
+        except Exception as e:
+            print(f"Error using GPU implementation: {e}")
+            return compute_prob_cpu(probs, mhk_minus, frequencies_primary_minus, frequencies_secondary,
+                          y_values, epsilon, a, b, max_clusters, degree_corrected,
+                          degree_cluster_minus, degree_node, degree_param, is_user_mode)
+    else:
+        return compute_prob_cpu(probs, mhk_minus, frequencies_primary_minus, frequencies_secondary,
+                          y_values, epsilon, a, b, max_clusters, degree_corrected,
+                          degree_cluster_minus, degree_node, degree_param, is_user_mode)
+        
+        
+
+@nb.njit(fastmath=True, parallel=False)
+def compute_prob_cpu(probs, 
+                          mhk_minus, 
+                          frequencies_primary_minus, 
+                          frequencies_secondary,
+                          y_values, 
+                          epsilon, 
+                          a, 
+                          b, 
+                          max_clusters, 
+                          degree_corrected,
+                          degree_cluster_minus, 
+                          degree_node, 
+                          degree_param, 
+                          is_user_mode):
+    """
+    CPU version of log probability computation for Gibbs sampling steps.
+    """
+    log_probs = np.zeros_like(probs)
+    a_plus_epsilon = a + epsilon
+    lgamma_a = lgamma(a)
+    log_b = np.log(b)
+    lgamma_a_log_b = -lgamma_a + a * log_b
+    
+    # Set indices based on mode
+    primary_range = range(max_clusters)
+    
+    for i in primary_range:
+        p_i = 0.0
+        freq_i = frequencies_primary_minus[i]
+        
+        for j in range(len(frequencies_secondary)):
+            # swap indices based on user/item mode
+            if is_user_mode:
+                h, k = i, j  
+            else:
+                k, h = i, j  
+            
+            mhk_val = mhk_minus[h, k]
+            y_val = y_values[j]  
+            
+            mhk_plus_a = mhk_val + a_plus_epsilon
+            mhk_plus_y_plus_a = mhk_val + y_val + a_plus_epsilon
+            
+            log_freq_prod1 = np.log(b + freq_i * frequencies_secondary[j])
+            log_freq_prod2 = np.log(b + (freq_i + 1) * frequencies_secondary[j])
+            
+            p_i += (lgamma(mhk_plus_y_plus_a) - lgamma(mhk_plus_a) +
+                   (mhk_plus_a - epsilon) * log_freq_prod1 -
+                   (mhk_plus_y_plus_a - epsilon) * log_freq_prod2)
+            
+        log_probs[i] += p_i
+        
+        if degree_corrected is True:
+            first = lgamma(frequencies_primary_minus[i]*degree_param + degree_cluster_minus[i])
+            second = lgamma((frequencies_primary_minus[i]+1)*degree_param+degree_cluster_minus[i]+degree_node)
+            
+            third = lgamma((frequencies_primary_minus[i]+1)*degree_param)
+            fourth = lgamma(frequencies_primary_minus[i]*degree_param)
+            
+            fifth = (degree_cluster_minus[i]+degree_node)*np.log(frequencies_primary_minus[i]+1)
+            sixth = degree_cluster_minus[i]*np.log(frequencies_primary_minus[i])
+            
+            log_probs[i] += (first - second + third - fourth + fifth - sixth)
+                        
+    # Handle new cluster case
+    if len(log_probs) > max_clusters:
+        p_new = 0.0
+        for j in range(len(frequencies_secondary)):
+            y_val = y_values[j]
+            p_new += (lgamma(y_val + a_plus_epsilon) + lgamma_a_log_b -
+                     (y_val + a) * np.log(b + frequencies_secondary[j]))
+                
+        log_probs[max_clusters] += p_new
+        if degree_corrected is True:
+            log_probs[max_clusters] += (lgamma(degree_param)- lgamma(degree_param+degree_node))
+    
+    return log_probs
+
+
+@nb.jit(nopython=True)
+def compute_log_probs_cov(probs, 
+                          idx, 
+                          cov_types, 
+                          cov_nch, 
+                          cov_values, 
+                          nh, 
+                          alpha_c, 
+                          alpha_0):
+    """Numba-optimized function to compute contribution of covariates to log probabilities.
+
+    Parameters
+    ----------
+    probs : array-like
+        array of probabilities
+    idx : int
+        index of the user/item being considered
+    cov_types : array-like
+        types of covariates ('cat' for categorical)
+    cov_nch : array-like
+        nch matrices for categorical covariates
+    cov_values : array-like
+        covariate values for each user/item
+    nh : array-like
+        cluster sizes
+    alpha_c : array-like
+        alpha_c parameters for categorical covariates
+    alpha_0 : float
+        sum of alpha_c parameters
+
+    Returns
+    -------
+    log_probs : array-like
+        log probabilities contribution from covariates
+    """
+    
+    log_probs = np.zeros_like(probs)
+    for i in nb.prange(len(cov_types)):
+        if cov_types[i]=='cat':
+            c = cov_values[i][idx]
+            nch = cov_nch[i]
+            for h in range(len(nh)):
+                log_probs[h] += np.log(nch[c, h]+alpha_c[c])-np.log(nh[h]+alpha_0)
+            log_probs[-1] += np.log(alpha_c[c])-np.log(alpha_0)
+    
+    return log_probs
+
+
+
+        
+     
---
+++ pyesbm/utilities/plotting_functions.py
@@ -0,0 +1,198 @@
+################################################
+# auxialiaries for plotting
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.patches import Rectangle
+
+# function to plot the heatmap representation
+def plot_heatmap(model, 
+                 user_covariates=None, 
+                 item_covariates=None, 
+                 add_labels=True, 
+                 size=(12,10), 
+                 capped=None,
+                 save_path=None):
+    
+    if user_covariates is not None:
+        user_covariates = user_covariates.copy()
+    if item_covariates is not None:
+        item_covariates = item_covariates.copy()
+
+    user_clustering = model.user_clustering.copy()
+    item_clustering = model.item_clustering.copy()
+    Y = model.Y
+    if capped is not None:
+        Y = np.clip(Y, 0, capped)
+
+    # Sort clusters by size
+    user_cluster_sizes = {}
+    for cluster in user_clustering:
+        user_cluster_sizes[cluster] = user_cluster_sizes.get(cluster, 0) + 1
+
+    item_cluster_sizes = {}
+    for cluster in item_clustering:
+        item_cluster_sizes[cluster] = item_cluster_sizes.get(cluster, 0) + 1
+
+    sorted_user_clusters = sorted(user_cluster_sizes.keys(),
+                                key=lambda x: user_cluster_sizes[x],
+                                reverse=True)
+    sorted_item_clusters = sorted(item_cluster_sizes.keys(),
+                                key=lambda x: item_cluster_sizes[x],
+                                reverse=True)
+
+    user_cluster_rank = {cluster: i for i, cluster in enumerate(sorted_user_clusters)}
+    item_cluster_rank = {cluster: i for i, cluster in enumerate(sorted_item_clusters)}
+
+    idx_sort_users = sorted(np.arange(Y.shape[0]),
+                        key=lambda i: user_cluster_rank[user_clustering[i]])
+    idx_sort_items = sorted(np.arange(Y.shape[1]),
+                        key=lambda i: item_cluster_rank[item_clustering[i]])
+
+    sorted_user_clusters_list = [user_clustering[i] for i in idx_sort_users]
+    sorted_item_clusters_list = [item_clustering[i] for i in idx_sort_items]
+
+    user_cluster_boundaries = [0]
+    prev_cluster = sorted_user_clusters_list[0]
+    for i, cluster in enumerate(sorted_user_clusters_list[1:], 1):
+        if cluster != prev_cluster:
+            user_cluster_boundaries.append(i)
+            prev_cluster = cluster
+    user_cluster_boundaries.append(len(sorted_user_clusters_list))  
+
+    item_cluster_boundaries = [0]
+    prev_cluster = sorted_item_clusters_list[0]
+    for i, cluster in enumerate(sorted_item_clusters_list[1:], 1):
+        if cluster != prev_cluster:
+            item_cluster_boundaries.append(i)
+            prev_cluster = cluster
+    item_cluster_boundaries.append(len(sorted_item_clusters_list))  
+
+    # Create figure with appropriate size and layout for covariates
+    fig = plt.figure(figsize=size)
+
+    # Set the layout based on whether we have covariates
+    if user_covariates is not None or item_covariates is not None:
+        width_ratios = [0.02, 1] if user_covariates is not None else [0, 1]
+        height_ratios = [1, 0.02] if item_covariates is not None else [1, 0]
+        gs = fig.add_gridspec(2, 2, width_ratios=width_ratios, 
+                                height_ratios=height_ratios,
+                                wspace=0.01, hspace=0.01)
+        
+        # Main heatmap
+        ax_heatmap = fig.add_subplot(gs[0, 1])
+        
+        # User covariates on the left
+        if user_covariates is not None:
+            ax_user_cov = fig.add_subplot(gs[0, 0], sharey=ax_heatmap)
+        
+        # Item covariates on the bottom
+        if item_covariates is not None:
+            ax_item_cov = fig.add_subplot(gs[1, 1], sharex=ax_heatmap)
+    else:
+        ax_heatmap = fig.add_subplot(111)
+
+    # Plot main heatmap
+    heatmap = sns.heatmap(Y[idx_sort_users, :][:, idx_sort_items], ax=ax_heatmap, cbar_kws={"shrink": 0.8})
+
+    # Add cluster boundaries
+    for boundary in user_cluster_boundaries:
+        ax_heatmap.axhline(y=boundary, color='white', linewidth=2)
+    for boundary in item_cluster_boundaries:
+        ax_heatmap.axvline(x=boundary, color='white', linewidth=2)
+
+    # Add titles and labels
+    # ax_heatmap.set_title('Heatmap with Largest Clusters in Top-Left Corner', fontsize=15, pad=20)
+    ax_heatmap.set_xlabel('Items', fontsize=14, labelpad=20)
+    ax_heatmap.set_ylabel('Users', fontsize=14, labelpad=20)
+
+    # Add cluster labels if requested
+    if add_labels:
+        for i in range(len(user_cluster_boundaries) - 1):
+            cluster_label = sorted_user_clusters[i]
+            mid_point = (user_cluster_boundaries[i] + user_cluster_boundaries[i+1]) / 2
+            ax_heatmap.text(-0.5, mid_point, f'C{cluster_label}',
+                    verticalalignment='center', horizontalalignment='right',
+                    fontsize=12)
+        
+        for i in range(len(item_cluster_boundaries) - 1):
+            cluster_label = sorted_item_clusters[i]
+            mid_point = (item_cluster_boundaries[i] + item_cluster_boundaries[i+1]) / 2
+            ax_heatmap.text(mid_point, Y.shape[0] + 0.5, f'C{cluster_label}',
+                    verticalalignment='top', horizontalalignment='center',
+                    fontsize=12)
+
+    ax_heatmap.tick_params(axis='both', which='both', bottom=False, top=False,
+                    labelbottom=False, left=False, right=False, labelleft=False)
+
+    # Process and plot user covariates if provided
+    if user_covariates is not None:
+        sorted_user_covs = [user_covariates[i] for i in idx_sort_users]
+        
+        # Get unique categories and assign colors
+        unique_user_cats = sorted(set(sorted_user_covs))
+        user_cmap = plt.get_cmap('tab10', len(unique_user_cats))
+        user_color_dict = {cat: user_cmap(i) for i, cat in enumerate(unique_user_cats)}
+        
+        # Create color matrix for user covariates
+        n_users = len(sorted_user_covs)
+        user_cov_matrix = np.zeros((n_users, 1))
+        for i, cov in enumerate(sorted_user_covs):
+            user_cov_matrix[i, 0] = unique_user_cats.index(cov)
+        
+        # Plot user covariates with proper alignment
+        # The extent parameter is set to match the axis limits of the heatmap
+        user_img = ax_user_cov.imshow(user_cov_matrix, aspect='auto', origin='upper', 
+                                    cmap=user_cmap, extent=[0, 1, n_users, 0])
+        
+        ax_user_cov.set_xticks([])
+        ax_user_cov.set_yticks([])
+        
+        # Add a legend for user covariates
+        user_legend_elements = [Rectangle((0, 0), 1, 1, color=user_color_dict[cat], label=str(cat)) 
+                                for cat in unique_user_cats]
+        ax_user_cov.legend(handles=user_legend_elements, title='User Covariates',
+                        bbox_to_anchor=(0.1, 0.1), loc='center right')
+
+    if item_covariates is not None:
+        # Extract covariates for sorted items
+        if isinstance(item_covariates, dict):
+            sorted_item_covs = [item_covariates[i] for i in idx_sort_items]
+        else: 
+            sorted_item_covs = [item_covariates[i] for i in idx_sort_items]
+        
+        # Get unique categories and assign colors
+        unique_item_cats = sorted(set(sorted_item_covs))
+        item_cmap = plt.get_cmap('tab10', len(unique_item_cats))
+        item_color_dict = {cat: item_cmap(i) for i, cat in enumerate(unique_item_cats)}
+        
+        # Create color matrix for item covariates
+        n_items = len(sorted_item_covs)
+        item_cov_matrix = np.zeros((1, n_items))
+        for i, cov in enumerate(sorted_item_covs):
+            item_cov_matrix[0, i] = unique_item_cats.index(cov)
+        
+        # Plot item covariates with proper alignment
+        # The extent parameter is set to match the axis limits of the heatmap
+        heatmap_pos = ax_heatmap.get_position()
+        ax_item_cov.set_position([heatmap_pos.x0, ax_item_cov.get_position().y0, 
+                                heatmap_pos.width, ax_item_cov.get_position().height])
+
+        # Plot item covariates ensuring correct width
+        item_img = ax_item_cov.imshow(item_cov_matrix, aspect='auto', origin='upper', cmap=item_cmap, 
+                                    extent=[0, n_items, 1, 0])
+
+        
+        ax_item_cov.set_xticks([])
+        ax_item_cov.set_yticks([])
+        
+        item_legend_elements = [Rectangle((0, 0), 1, 1, color=item_color_dict[cat], label=str(cat)) 
+                                for cat in unique_item_cats]
+        ax_item_cov.legend(handles=item_legend_elements, title='Item Covariates',
+                            bbox_to_anchor=(0.25, -5), loc='center right')    
+
+    if save_path is not None:
+        plt.savefig(save_path, bbox_inches='tight')
+    else:
+        plt.show()
